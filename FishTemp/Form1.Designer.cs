87c8635f23ef05140a99f400379fa363a05aa21e
Давлетшин Булат Айратович <212028@mck-ktits.ru>
2022-06-25 15:41:16 +0300

    Добавьте файлы проекта.

---
+++ FishTemp/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FishTemp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Run = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.FishName = new System.Windows.Forms.ComboBox();
+            this.Temperature = new System.Windows.Forms.TextBox();
+            this.DataTime = new System.Windows.Forms.TextBox();
+            this.TimeMin = new System.Windows.Forms.TextBox();
+            this.MinTemp = new System.Windows.Forms.TextBox();
+            this.TimeMax = new System.Windows.Forms.TextBox();
+            this.MaxTemp = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.Controls.Add(this.Run);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnOpen);
+            this.panel1.Controls.Add(this.FishName);
+            this.panel1.Controls.Add(this.Temperature);
+            this.panel1.Controls.Add(this.DataTime);
+            this.panel1.Controls.Add(this.TimeMin);
+            this.panel1.Controls.Add(this.MinTemp);
+            this.panel1.Controls.Add(this.TimeMax);
+            this.panel1.Controls.Add(this.MaxTemp);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(425, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(159, 326);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 11;
+            this.Run.Text = "Результат";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(327, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(211, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 9;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // FishName
+            // 
+            this.FishName.FormattingEnabled = true;
+            this.FishName.Items.AddRange(new object[] {
+            "Семга",
+            "Минтай"});
+            this.FishName.Location = new System.Drawing.Point(12, 38);
+            this.FishName.Name = "FishName";
+            this.FishName.Size = new System.Drawing.Size(147, 23);
+            this.FishName.TabIndex = 8;
+            this.FishName.TextChanged += new System.EventHandler(this.FishName_TextChanged);
+            // 
+            // Temperature
+            // 
+            this.Temperature.Location = new System.Drawing.Point(12, 276);
+            this.Temperature.Name = "Temperature";
+            this.Temperature.PlaceholderText = "Температура";
+            this.Temperature.Size = new System.Drawing.Size(378, 23);
+            this.Temperature.TabIndex = 6;
+            // 
+            // DataTime
+            // 
+            this.DataTime.Location = new System.Drawing.Point(12, 185);
+            this.DataTime.Name = "DataTime";
+            this.DataTime.PlaceholderText = "Дата";
+            this.DataTime.Size = new System.Drawing.Size(147, 23);
+            this.DataTime.TabIndex = 5;
+            // 
+            // TimeMin
+            // 
+            this.TimeMin.Location = new System.Drawing.Point(211, 121);
+            this.TimeMin.Name = "TimeMin";
+            this.TimeMin.PlaceholderText = "Допустимое время";
+            this.TimeMin.Size = new System.Drawing.Size(126, 23);
+            this.TimeMin.TabIndex = 4;
+            // 
+            // MinTemp
+            // 
+            this.MinTemp.Location = new System.Drawing.Point(12, 121);
+            this.MinTemp.Name = "MinTemp";
+            this.MinTemp.PlaceholderText = "Мин температура";
+            this.MinTemp.Size = new System.Drawing.Size(147, 23);
+            this.MinTemp.TabIndex = 3;
+            // 
+            // TimeMax
+            // 
+            this.TimeMax.Location = new System.Drawing.Point(211, 76);
+            this.TimeMax.Name = "TimeMax";
+            this.TimeMax.PlaceholderText = "Допустимое время";
+            this.TimeMax.Size = new System.Drawing.Size(126, 23);
+            this.TimeMax.TabIndex = 2;
+            // 
+            // MaxTemp
+            // 
+            this.MaxTemp.Location = new System.Drawing.Point(12, 76);
+            this.MaxTemp.Name = "MaxTemp";
+            this.MaxTemp.PlaceholderText = "Макс температура";
+            this.MaxTemp.Size = new System.Drawing.Size(147, 23);
+            this.MaxTemp.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(425, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(470, 450);
+            this.panel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox TimeMax;
+        private System.Windows.Forms.TextBox MaxTemp;
+        private System.Windows.Forms.TextBox me;
+        private System.Windows.Forms.TextBox Temperature;
+        private System.Windows.Forms.TextBox DataTime;
+        private System.Windows.Forms.TextBox TimeMin;
+        private System.Windows.Forms.TextBox MinTemp;
+        private System.Windows.Forms.ComboBox FishName;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ FishTemp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FishTemp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FishName_TextChanged(object sender, EventArgs e)
+        {
+            if (FishName.Text == "Минтай")
+            {
+                MinTemp.Visible = false;
+                TimeMin.Visible = false;
+            }
+            else
+            {
+                MaxTemp.Visible = true;
+                TimeMax.Visible = true;
+                MinTemp.Visible = true;
+                TimeMin.Visible = true;
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(ofd.FileName))
+                {
+                    List<string> list = new List<string>();
+                    string line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        DataTime.Text = Convert.ToDateTime(line).ToString("yy.mm.dd h:mm");
+                        line = sr.ReadLine();
+                        Temperature.Text = line;
+                    }
+                }
+            }
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            switch (FishName.Text)
+            {
+                case "Сенга"
+
+            }
+        }
+    }
+}
